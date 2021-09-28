f6e363f46db73317f84ad6434ea9bee34fbe7db2
ciyabox <77894626+ciyabox@users.noreply.github.com>
2021-09-28 22:22:56 +0300

    Update splash_screen.dart

--- lib/UI/screens/splash_screen.dart
+++ lib/UI/screens/splash_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import '../../UI/screens/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   static const route = "SplashScreen";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Splash Screen")),
+    return WillPopScope(
+      onWillPop: () => Future.value(false),
+      child: Scaffold(
+        body: FutureBuilder<bool>(
+          future: businessFunction(),
+          builder: (_, snapshot) {
+            if (snapshot.hasData)
+              WidgetsBinding.instance?.addPostFrameCallback(
+                (_) => Navigator.pushReplacementNamed(context, HomeScreen.route),
+              );
+            return Center(child: Text(route));
+          },
+        ),
+      ),
     );
+  }
+
+  Future<bool> businessFunction() async {
+    await Future.delayed(Duration(seconds: 3));
+    return true;
   }
 }

52f2745479483318c41872c75c6dd020e43c3326
ciyabox <77894626+ciyabox@users.noreply.github.com>
2021-10-09 18:02:07 +0300

    Update store_manager.dart

--- lib_mobx-route/Business/managers/store_manager.dart
+++ lib_mobx-route/Business/managers/store_manager.dart
@@ -1,9 +1,8 @@
-import 'package:mobx_system_demo/Business/state_management/mobx/store/local_store.dart';
+import 'package:mobx_system_demo/Business/state_management/mobx/local_store.dart';
 
 final storeManager = StoreManager();
-class StoreManager{
 
+class StoreManager {
   LocalStore? _localStore;
   LocalStore get localStore => _localStore ??= LocalStore();
-
 }

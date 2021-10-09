d00e45acf38345e2cf328dd96da39c0b001ff6a9
ciyabox <77894626+ciyabox@users.noreply.github.com>
2021-10-09 18:00:53 +0300

    Create extensions.dart

---
+++ lib_mobx-route/Business/helpers/extensions/extensions.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext{
+  MediaQueryData get getMediaQuery => MediaQuery.of(this);
+  ThemeData get getTheme => Theme.of(this);
+}

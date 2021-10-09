8aa0720241aa8ec44514693e3888c488bee9de1f
ciyabox <77894626+ciyabox@users.noreply.github.com>
2021-10-09 18:16:55 +0300

    Delete lib_mobx-route directory

--- lib_mobx-route/Business/helpers/extensions.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension ContextExtension on BuildContext{
-  MediaQueryData get getMediaQuery => MediaQuery.of(this);
-  ThemeData get getTheme => Theme.of(this);
-}
--- lib_mobx-route/Business/managers/store_manager.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mobx_system_demo/Business/state_management/mobx/local_store.dart';
-
-final storeManager = StoreManager();
-
-class StoreManager {
-  LocalStore? _localStore;
-  LocalStore get localStore => _localStore ??= LocalStore();
-}

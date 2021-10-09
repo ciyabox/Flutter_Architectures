2074efbf9f4c1c73e8eeb1a343c6e60f75da9ae7
ciyabox <77894626+ciyabox@users.noreply.github.com>
2021-10-09 18:06:49 +0300

    Update home_screen.dart

--- lib_mobx-route/UI/screens/home_screen.dart
+++ lib_mobx-route/UI/screens/home_screen.dart
@@ -1,48 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx_system_demo/Business/managers/store_manager.dart';
+import 'package:mobx_system_demo/Business/state_management/notifier/global_notifier.dart';
+import 'package:mobx_system_demo/UI/widgets/screen_widgets/home_screen_widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const route = "HomeScreen";
 
+  final homeScreenWidgets = HomeScreenWidgets();
+  final tempNotifier = GlobalNotifier<int>(11);
+
+  init() {
+    storeManager.localStore..getFutureValue();
+  }
+
   @override
   Widget build(BuildContext context) {
+    init();
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text("Home")),
-      body: Column(
-        children: [
-          Observer(
-            builder: (_) {
-              return Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: Text(
-                      storeManager.localStore.valueInt.toString(),
-                    )),
-                    Expanded(
-                      child: IconButton(
-                        onPressed: () {
-                          storeManager.localStore.valueInt += 1;
-                        },
-                        icon: Icon(Icons.plus_one),
-                      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Observer(
+              builder: (context) => Row(
+                children: [
+                  Expanded(
+                    child: homeScreenWidgets.textWidget(
+                      text: storeManager.localStore.futureValue.toString(),
+                      margin: const EdgeInsets.all(4),
                     ),
-                    Expanded(
-                      child: IconButton(
-                        onPressed: () {
-                          storeManager.localStore.valueInt -= 1;
-                        },
-                        icon: Icon(Icons.exposure_minus_1),
-                      ),
+                  ),
+                  Expanded(
+                    child: homeScreenWidgets.iconButtonWidget(
+                      icon: Icons.exposure_minus_1,
+                      onPressed: () => storeManager.localStore.value -= 1,
                     ),
-                  ],
+                  ),
+                  Expanded(
+                    child: homeScreenWidgets.textWidget(
+                      text: storeManager.localStore.value.toString(),
+                      margin: const EdgeInsets.all(4),
+                    ),
+                  ),
+                  Expanded(
+                    child: homeScreenWidgets.iconButtonWidget(
+                      icon: Icons.plus_one,
+                      onPressed: () => storeManager.localStore.value += 1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Spacer(),
+                Expanded(
+                  child: homeScreenWidgets.iconButtonWidget(
+                    icon: Icons.exposure_minus_1,
+                    onPressed: () {
+                      tempNotifier.value -= 1;
+                    },
+                  ),
                 ),
-              );
-            },
-          ),
-        ],
+                Expanded(
+                  child: ValueListenableBuilder<int>(
+                    valueListenable: tempNotifier,
+                    builder: (context, value, child) =>
+                        homeScreenWidgets.textWidget(
+                      text: value.toString(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: homeScreenWidgets.iconButtonWidget(
+                    icon: Icons.plus_one,
+                    onPressed: () {
+                      tempNotifier.value += 1;
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
